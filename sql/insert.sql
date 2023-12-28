e774a0c64d1b9f2fd2df9bc6b31bd95beaabf5d4
Elías <elias.ludviksson@gmail.com>
2023-12-28 21:22:17 +0000

    keyrandi 2022 utgafa

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,1 +1,10 @@
--- Útfæra test gögn
+INSERT INTO events (name, slug, description) VALUES ('Forritarahittingur í febrúar', 'forritarahittingur-i-februar', 'Forritarar hittast í febrúar og forrita saman eitthvað frábært.');
+INSERT INTO events (name, slug, description) VALUES ('Hönnuðahittingur í mars', 'honnudahittingur-i-mars', 'Spennandi hittingur hönnuða í Hönnunarmars.');
+INSERT INTO events (name, slug, description) VALUES ('Verkefnastjórahittingur í apríl', 'verkefnastjorahittingur-i-april', 'Virkilega vel verkefnastýrður hittingur.');
+
+INSERT INTO registrations (name, comment, event) VALUES ('Forvitinn forritari', 'Hlakka til að forrita með ykkur', 1);
+INSERT INTO registrations (name, comment, event) VALUES ('Jón Jónsson', null, 1);
+INSERT INTO registrations (name, comment, event) VALUES ('Guðrún Guðrúnar', 'verður vefforritað?', 1);
+
+INSERT INTO users (username, password) VALUES ('admin', '$2b$11$Bp2oyUW2ewLM6xu7F01EquBq8gP5drCM/tboNT0K3ApsTLan5W3Re');
+INSERT INTO users (username, password) VALUES ('a', '$2b$11$m/SLw5N9J0OopC5a5s5dXuUScLO/zxTgYHXwGfDVACD/iMR7DwSOa');
--- src/lib/db.js
+++ src/lib/db.js
@@ -5,11 +5,11 @@ const SCHEMA_FILE = './sql/schema.sql';
 const DROP_SCHEMA_FILE = './sql/drop.sql';
 
 const { DATABASE_URL: connectionString, NODE_ENV: nodeEnv = 'development' } =
-  process.env;
+	process.env;
 
 if (!connectionString) {
-  console.error('vantar DATABASE_URL í .env');
-  process.exit(-1);
+	console.error('vantar DATABASE_URL í .env');
+	process.exit(-1);
 }
 
 // Notum SSL tengingu við gagnagrunn ef við erum *ekki* í development
@@ -19,46 +19,175 @@ const ssl = nodeEnv === 'production' ? { rejectUnauthorized: false } : false;
 const pool = new pg.Pool({ connectionString, ssl });
 
 pool.on('error', (err) => {
-  console.error('Villa í tengingu við gagnagrunn, forrit hættir', err);
-  process.exit(-1);
+	console.error('Villa í tengingu við gagnagrunn, forrit hættir', err);
+	process.exit(-1);
 });
 
 export async function query(q, values = []) {
-  let client;
-  try {
-    client = await pool.connect();
-  } catch (e) {
-    console.error('unable to get client from pool', e);
-    return null;
-  }
+	let client;
+	try {
+		client = await pool.connect();
+	} catch (e) {
+		console.error('unable to get client from pool', e);
+		return null;
+	}
 
-  try {
-    const result = await client.query(q, values);
-    return result;
-  } catch (e) {
-    if (nodeEnv !== 'test') {
-      console.error('unable to query', e);
-    }
-    return null;
-  } finally {
-    client.release();
-  }
+	try {
+		const result = await client.query(q, values);
+		return result;
+	} catch (e) {
+		if (nodeEnv !== 'test') {
+			console.error('unable to query', e);
+		}
+		return null;
+	} finally {
+		client.release();
+	}
 }
 
 export async function createSchema(schemaFile = SCHEMA_FILE) {
-  const data = await readFile(schemaFile);
+	const data = await readFile(schemaFile);
 
-  return query(data.toString('utf-8'));
+	return query(data.toString('utf-8'));
 }
 
 export async function dropSchema(dropFile = DROP_SCHEMA_FILE) {
-  const data = await readFile(dropFile);
+	const data = await readFile(dropFile);
 
-  return query(data.toString('utf-8'));
+	return query(data.toString('utf-8'));
 }
 
 export async function end() {
-  await pool.end();
+	await pool.end();
 }
 
 /* TODO útfæra aðgeðir á móti gagnagrunni */
+export async function listEvents() {
+	const q = `
+	  SELECT
+		id, name, slug, description, created, updated
+	  FROM
+		events
+	`;
+
+	const result = await query(q);
+
+	if (result) {
+		return result.rows;
+	}
+
+	return null;
+}
+
+export async function listEvent(slug) {
+	const q = `
+	SELECT
+		id, name, slug, description, created, updated
+	FROM
+		events
+	WHERE slug = $1
+	`;
+
+	const result = await query(q, [slug]);
+
+	if (result && result.rowCount === 1) {
+		return result.rows[0];
+	}
+
+	return null // gekk ekki
+}
+
+export async function listRegistered(event) {
+	const q = `
+	  SELECT
+		id, name, comment
+	  FROM
+		registrations
+	  WHERE event = $1
+	`;
+
+	const result = await query(q, [event]);
+
+	if (result) {
+		return result.rows;
+	}
+
+	return null;
+}
+// skráir þig á viðburð
+export async function register({ name, comment, event } = {}) {
+	const q = `
+	  INSERT INTO registrations
+		(name, comment, event)
+	  VALUES
+		($1, $2, $3)
+	  RETURNING
+		id, name, comment, event;
+	`;
+	const values = [name, comment, event];
+	const result = await query(q, values);
+
+	if (result && result.rowCount === 1) {
+		return result.rows[0];
+	}
+
+	return null;
+}
+
+export async function createEvent({ name, slug, description } = {}) {
+	const q = `
+	  INSERT INTO events
+		(name, slug, description)
+	  VALUES
+		($1, $2, $3)
+	  RETURNING id, name, slug, description;
+	`;
+	const values = [name, slug, description];
+	const result = await query(q, values);
+	console.log(result)
+	if (result && result.rowCount === 1) {
+		return result.rows[0];
+	}
+
+	return null;
+}
+export async function listEventByName(name) {
+	const q = `
+	  SELECT
+		id, name, slug, description, created, updated
+	  FROM
+		events
+	  WHERE name = $1
+	`;
+
+	const result = await query(q, [name]);
+
+	if (result && result.rowCount === 1) {
+		return result.rows[0];
+	}
+
+	return null;
+}
+
+// Updatear ekki description, erum ekki að útfæra partial update
+export async function updateEvent(id, { name, slug, description } = {}) {
+	const q = `
+	  UPDATE events
+		SET
+		  name = $1,
+		  slug = $2,
+		  description = $3,
+		  updated = CURRENT_TIMESTAMP
+	  WHERE
+		id = $4
+	  RETURNING id, name, slug, description;
+	`;
+	const values = [name, slug, description, id];
+	const result = await query(q, values);
+
+	if (result && result.rowCount === 1) {
+		return result.rows[0];
+	}
+
+	return null;
+}
