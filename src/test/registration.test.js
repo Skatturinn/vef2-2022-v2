4c4be1c9436a460ce6bd3234006ce2b970c9afbe
Elías <elias.ludviksson@gmail.com>
2024-01-01 18:08:35 +0000

    test env variables i brengli, nær ekki tengingu.

--- src/test/registration.test.js
+++ src/test/registration.test.js
@@ -1,50 +1,46 @@
 import { describe, expect, it } from '@jest/globals';
 import { validationResult } from 'express-validator';
 import {
-  registrationValidationMiddleware,
-  xssSanitizationMiddleware,
-} from '../routes/index-routes';
+	registrationValidationMiddleware,
+	xssSanitizationMiddleware,
+} from '../lib/validation';
 
-// Hjálparfall sem leyfir okkur að testa express-validator middleware
 // https://stackoverflow.com/questions/28769200/unit-testing-validation-with-express-validator
 async function applyAllMiddlewares(req, middlewares) {
-  await Promise.all(
-    middlewares.map(async (middleware) => {
-      await middleware(req, {}, () => undefined);
-    })
-  );
+	await Promise.all(
+		middlewares.map(async (middleware) => {
+			await middleware(req, {}, () => undefined);
+		})
+	);
 }
 
-// TODO breyta og laga test
-
 describe('registration', () => {
-  it('validates', async () => {
-    const req = {
-      body: {
-        name: '',
-      },
-    };
+	it('validates', async () => {
+		const req = {
+			body: {
+				name: '',
+			},
+		};
 
-    const registrationValidationMiddleware = []; // TODO Sækja á réttan stað
+		await applyAllMiddlewares(
+			req,
+			registrationValidationMiddleware('description')
+		);
 
-    await applyAllMiddlewares(req, );
+		const validation = validationResult(req);
 
-    const validation = validationResult(req);
+		expect(validation.isEmpty()).toBe(false);
+	});
 
-    expect(validation.isEmpty()).toBe(false);
-  });
+	it('sanitizes', async () => {
+		const req = {
+			body: {
+				name: '<script>alert(1)</script>',
+			},
+		};
 
-  it('sanitizes', async () => {
-    const req = {
-      body: {
-        name: '<script>alert(1)</script>',
-      },
-    };
+		await applyAllMiddlewares(req, xssSanitizationMiddleware('description'));
 
-    const xssSanitizationMiddleware = []; // TODO Sækja á réttan stað
-
-    await applyAllMiddlewares(req, xssSanitizationMiddleware);
-
-    expect(req.body.name).toBe('&lt;script&gt;alert(1)&lt;/script&gt;');
-  });
+		expect(req.body.name).toBe('&lt;script&gt;alert(1)&lt;/script&gt;');
+	});
 });
